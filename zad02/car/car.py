9fcfd46cbc0080e9e1f5b0408d645e2d8c373ced
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-07 13:16:59 +0100

    import nie działa

--- zad02/car/car.py
+++
@@ -1,9 +0,0 @@
-class Car:
-    def needsFuel(self):
-        pass
-
-    def getEngineTemperature(self):
-        pass
-
-    def driveTo(self, destination):
-        pass
--- zad02/tests/test_car.py
+++ zad02/tests/test_car.py
@@ -1,30 +1,20 @@
-from ..car.carImpl import CarImpl
+import sys
 
+sys.path.append('../src')
+from car import Car
+from carImpl import CarImpl
 from unittest.mock import Mock, patch
 import unittest
 
 
 class TestCar(unittest.TestCase):
-    @patch("..car.Car.driveTo")
-    @patch("..car.Car.needsFuel")
-    def test_willIArive_True(self, needsFuel, driveTo):
-        needsFuel.return_value = 300
-        driveTo.return_value = 250
+
+    def test_willIArive_True(self):
+        car = Car()
+        car.needsFuel = Mock(name='needsFuel')
+        car.needsFuel.return_value = False
+        car.driveTo = Mock(name='needsFuel')
+        car.driveTo.return_value = 250
+
         carImpl = CarImpl()
         self.assertEqual(carImpl.willIArive(250), True)
-
-    @patch("..car.Car.driveTo")
-    @patch("..car.Car.needsFuel")
-    def test_willIArive_False(self, needsFuel, driveTo):
-        needsFuel.return_value = 100
-        driveTo.return_value = 250
-        carImpl = CarImpl()
-        self.assertEqual(carImpl.willIArive(250), False)
-
-    @patch("..car.Car.needsFuel")
-    @patch("..car.Car.getEngineTemperature")
-    def report(self, needsFuel, getEngineTemperature):
-        needsFuel.return_value = 300
-        getEngineTemperature.return_value = 180
-        carImpl = CarImpl()
-        self.assertEqual(carImpl.report(), (300, 180))
