0dc0691b4be2560c4f3a19e820db12e82eccef70
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-07 23:59:44 +0100

    jednak działa ;-)

--- zad02/src/carImpl.py
+++ zad02/src/carImpl.py
@@ -2,15 +2,20 @@ from car import Car
 
 
 class CarImpl:
-    def __init__(self):
-        self.car = Car()
+    def __init__(self, car: Car):
+        self.car = car
 
     def willIArive(self, destination):
-        if self.car.driveTo(destination) < 600 and self.car.needsFuel():
-            return True
-        else:
-            return False
+        if self.car.driveTo(destination):
+            return "You drive to {}".format(str(self.car.driveTo(destination)))
+        return "Where are you driving?"
 
-    def report(self):
-        data = (self.car.needsFuel(), self.car.getEngineTemperature())
-        return data
+    def alert(self):
+        if self.car.needsFuel():
+            return "NEED FUEL"
+        return "It's ok"
+
+    def getTemp(self):
+        if self.car.getEngineTemperature():
+            return str(self.car.getEngineTemperature()) + " F"
+        return "404 Engine Not Found"
--- zad02/tests/test_car.py
+++ zad02/tests/test_car.py
@@ -9,12 +9,44 @@ import unittest
 
 class TestCar(unittest.TestCase):
 
-    def test_willIArive_True(self):
+    def test_willIArive_Poland(self):
+        car = Car()
+        car.driveTo = Mock(name='driveTo')
+        car.driveTo.return_value = "Poland"
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.willIArive("Poland"), "You drive to Poland")
+
+    def test_willIArive_False(self):
+        car = Car()
+        car.driveTo = Mock(name='driveTo')
+        car.driveTo.return_value = False
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.willIArive("Poland"), "Where are you driving?")
+
+    def test_alert_True(self):
+        car = Car()
+        car.needsFuel = Mock(name='needsFuel')
+        car.needsFuel.return_value = True
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.alert(), "NEED FUEL")
+
+    def test_alert_False(self):
         car = Car()
         car.needsFuel = Mock(name='needsFuel')
         car.needsFuel.return_value = False
-        car.driveTo = Mock(name='needsFuel')
-        car.driveTo.return_value = 250
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.alert(), "It's ok")
 
-        carImpl = CarImpl()
-        self.assertEqual(carImpl.willIArive(250), True)
+    def test_getTemp_360(self):
+        car = Car()
+        car.getEngineTemperature = Mock(name='getEngineTemperature')
+        car.getEngineTemperature.return_value = 360
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.getTemp(), "360 F")
+
+    def test_getTemp_False(self):
+        car = Car()
+        car.getEngineTemperature = Mock(name='getEngineTemperature')
+        car.getEngineTemperature.return_value = False
+        carImpl = CarImpl(car)
+        self.assertEqual(carImpl.getTemp(), "404 Engine Not Found")
